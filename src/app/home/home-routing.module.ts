c2938436e5a7a70ea204dcea6e6671663aa8b45f
caueeugenio <caue_eugenio@hotmail.com>
2021-07-19 23:25:49 -0300

    implementacao pagina novo usuario | refact

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 const routes: Routes = [
   {
@@ -13,6 +14,10 @@ const routes: Routes = [
         component: LoginComponent,
         //qnd for uma rota vazia ele vai projetar o component login
       },
+      {
+        path: 'novo-usuario',
+        component: NovoUsuarioComponent
+      }
     ],
   },
 ];
